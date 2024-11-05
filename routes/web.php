92610bc5217ce1e47673976b5573fddba458465d
DeveLoopzz <develoopzz00@gmail.com>
2024-11-05 21:29:09 +0100

    developer conflict

--- routes/web.php
+++ routes/web.php
@@ -7,15 +7,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class);
 
-//ArmorRoutes
-Route::get('/armors', ArmorController::class, 'read');
-Route::get('/armors/create', ArmorController::class, 'create');
-Route::get('/armors/edit', ArmorController::class, 'update');
-Route::get('/armors/delete', ArmorController::class, 'delete');
+Route::get('/armors', [ArmorController::class, 'read']);
+Route::get('/armors/create', [ArmorController::class, 'create']);
+Route::get('/armors/update', [ArmorController::class, 'update']);
+Route::get('/armors/delete', [ArmorController::class, 'delete']);
 
 //ItemRoutes
-Route::get('/items', ItemController::class, 'read');
-Route::get('/items/create', ItemController::class, 'create');
-Route::get('/items/edit', ItemController::class, 'update');
-Route::get('/items/delete', ItemController::class, 'delete');
+Route::get('/items', [ItemController::class, 'read']);
+Route::get('/items/create', [ItemController::class, 'create']);
+Route::get('/items/update', [ItemController::class, 'update']);
+Route::get('/items/delete', [ItemController::class, 'delete']);
 
