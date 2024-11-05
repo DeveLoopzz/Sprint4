6f8b61e363683bfcba4ccfa8860204d8b35f8326
DeveLoopzz <develoopzz00@gmail.com>
2024-11-05 16:34:00 +0100

    Create add to routes

--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,13 @@ Route::get('/', HomeController::class);
 
 //ArmorRoutes
 Route::get('/armors', ArmorController::class, 'read');
+Route::get('/armor/create', ArmorController::class, 'create');
 Route::get('/armors/edit/{armor}', ArmorController::class, 'update');
 Route::get('/armors/delete/{armor}', ArmorController::class, 'delete');
 
 //ItemRoutes
 Route::get('/items', ItemController::class, 'read');
+Route::get('/items/create', ItemController::class, 'create');
 Route::get('/items/edit/{item}', ItemController::class, 'update');
 Route::get('/items/delete/{item}', ItemController::class, 'delete');
 
