071e074938f2caa10359cc8b01cc383e71df2b32
DeveLoopzz <develoopzz00@gmail.com>
2024-11-05 22:27:46 +0100

    mutadores para resistencias y armaduras

--- app/Models/Armor.php
+++ app/Models/Armor.php
@@ -9,6 +9,7 @@ class Armor extends Model
 {
     protected $table = 'armor';
 
+    //MUTADOR MAXIMA ARMADURA
     protected function armadura() : Attribute
     {
         return Attribute::make(
@@ -19,14 +20,13 @@ class Armor extends Model
         );
     }
 
+    //MUTADORES RESISTENCIAS
     protected function res_fuego(): Attribute
     {
         return Attribute::make(
             set: fn($resFuego) => $this->resRange($resFuego)
         );
     }
-    
-    
     protected function res_agua(): Attribute
     {
         return Attribute::make(
@@ -50,5 +50,12 @@ class Armor extends Model
         return Attribute::make(
             set: fn($resDraco) => $this->resRange($resDraco)
         );
+    }
+
+    protected function resRange($value)
+    {
+        //Min(5,$value) devuelve 5 si $value es mayor que 5(valor minimo) y max(-5, __lo_que_devuelva_min)
+        //devolvera -5 si $value es menor
+        return max(-5, min(5, $value)); 
     }
 }
