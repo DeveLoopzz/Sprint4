77f7b0cdeaa58ab220747be91aaedc67e2467868
DeveLoopzz <develoopzz00@gmail.com>
2024-11-06 11:00:55 +0100

    Item store functionality

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Item;
 use Illuminate\Http\Request;
 
 class ItemController extends Controller
@@ -14,6 +15,18 @@ class ItemController extends Controller
     public function create()
     {
         return view('Armor.create');
+    }
+    public function store(Request $request) 
+    {
+        $item = new Item();
+        $item->nombre = $request->nombre;
+        $item->descripcion = $request->descripcion;
+        $item->metodo_obtencion = $request->metodo_obtencion;
+        $item->rareza = $request->rareza;
+
+        $item->save();
+
+        return redirect('/items');
     }
 
     public function update() 
