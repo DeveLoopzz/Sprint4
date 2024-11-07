654494d84c77fadeee3b4f002d032b0701f066ba
DeveLoopzz <develoopzz00@gmail.com>
2024-11-07 13:08:23 +0100

    minor changes

--- resources/views/Item/read.blade.php
+++ resources/views/Item/read.blade.php
@@ -1,27 +1,36 @@
 <!DOCTYPE html>
-
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
 <body>
-        @foreach ($itemList as $item)
-        <div class= "tarjeta" style="border: 2px solid black;">
-            <a href="/items/update/{{$item->id}}">
-                <h3>
-                    {{$item->nombre}}
-                </h3>
-                <p>
-                    {{$item->descripcion}}
-                </p>
-                <p>
-                    {{$item->metodo_obtencion}}
-                </p>
-                <p>
-                    {{$item->rareza}}
-                </p>
-            </a>
-        </div>
-        @endforeach
-        <div class="tarjeta" style="border 2px solid black;">
-            <a href="/items/create">
-                <h3>Create new item</h3>
-            </a>
-        </div>
+    @foreach ($itemList as $item)
+    <div class= "tarjeta" style="border: 2px solid black;">
+        <a href="/items/update/{{$item->id}}">
+            <h3>
+                {{$item->nombre}}
+            </h3>
+            <p>
+                {{$item->descripcion}}
+            </p>
+            <p>
+                {{$item->metodo_obtencion}}
+            </p>
+            <p>
+                {{$item->rareza}}
+            </p>
+        </a>
+    </div>
+    @endforeach
+    <div class="tarjeta" style="border: 2px solid black;">
+        <a href="/items/create">
+            <h3>Create new item</h3>
+        </a>
+    </div>
 </body>    
+
+
+</html>
