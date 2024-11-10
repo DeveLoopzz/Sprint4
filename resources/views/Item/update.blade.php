48e23766a35eb2bd6de27e2724f1ad6064ee63b7
DeveLoopzz <develoopzz00@gmail.com>
2024-11-10 18:47:17 +0100

    Update item function

--- resources/views/Item/update.blade.php
+++ resources/views/Item/update.blade.php
@@ -1,1 +1,43 @@
-<h1>Aqui va la vista del update item</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Aqui va la vista del update item</h1>
+    <form action="/items" method="POST">
+        @csrf
+        <label>
+            Nombre:
+            <input type="text" name="nombre" value= "{{$item->nombre}}">
+        </label>
+        <label>
+            Descripción:
+            <textarea name="descripcion"> {{$item->descripcion}}</textarea>
+        </label>
+        <label>
+            Metodo de obtención:
+            <select name="metodo_obtencion">
+                @foreach ($metodosObtencion as $metodo)
+                    <option value="{{$metodo}}" {{$metodo == $item->metodo_obtencion ? 'selected' : ''}}>{{ucfirst($metodo)}}</option>
+                @endforeach
+            </select>
+        </label>
+        <label>
+            Rareza:
+            <select name="rareza" value="{{$item->rareza}}">
+                @foreach ($rarezas as $rareza)
+                    <option value="{{$rareza}}" {{$rareza == $item->rareza ? 'selected' : ''}}>{{ucfirst($rareza)}}</option>
+                @endforeach
+            </select>
+        </label>
+
+        <button type="submit">
+            Create Item
+        </button>
+    </form>
+</body>
+</html>
